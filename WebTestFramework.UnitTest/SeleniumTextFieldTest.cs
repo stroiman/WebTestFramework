c5e7b46c7ee928c33c69ee881af468c71d267bb5
Peter Stroiman <peter.s@me.com>
2010-04-05 21:35:34 +0200

    Created SeleniumTextField class
    The SeleniumTextField class controls the selenium behaviour relevant to
    a specific text field on a form.
    
    Signed-off-by: Peter Stroiman <peter.s@me.com>

---
+++ WebTestFramework.UnitTest/SeleniumTextFieldTest.cs
@@ -0,0 +1,36 @@
+﻿using Moq;
+using NUnit.Framework;
+using Selenium;
+
+namespace WebTestFramework.UnitTest
+{
+	[TestFixture]
+	public class SeleniumTextFieldTest
+	{
+		private string _controlID;
+		private string _controlLocator;
+
+		[SetUp]
+		public void Setup()
+		{
+			_controlID = "textBoxID";
+			_controlLocator = "id=" + _controlID;
+		}
+
+		[Test]
+		public void Type()
+		{
+			// Setup
+			const string value = "value";
+			var seleniumMock = new Mock<ISelenium>(MockBehavior.Strict);
+			seleniumMock.Setup(x => x.Type(_controlLocator, value));
+			var textBox = new SeleniumTextField(seleniumMock.Object, _controlID);
+
+			// Exercise
+			textBox.Type(value);
+
+			// Validate
+			seleniumMock.VerifyAll();
+		}
+	}
+}
---
+++ WebTestFramework/SeleniumTextField.cs
@@ -0,0 +1,42 @@
+﻿using Selenium;
+
+namespace WebTestFramework
+{
+	/// <summary>
+	/// A class that uses Selenium RC to control a text field on a web page form.
+	/// </summary>
+	public class SeleniumTextField
+	{
+		private readonly ISelenium _selenium;
+		private readonly string _controlID;
+
+		/// <summary>
+		/// Creates a new <see cref="SeleniumTextField"/> instance.
+		/// </summary>
+		/// <param name="selenium">
+		/// A reference to the <see cref="ISelenium"/> implementation to use
+		/// </param>
+		/// <param name="controlID">
+		/// The ID of the text field to control
+		/// </param>
+		public SeleniumTextField(ISelenium selenium, string controlID)
+		{
+			_selenium = selenium;
+			_controlID = controlID;
+		}
+
+		private string Locator { get { return "id=" + _controlID; } }
+
+		/// <summary>
+		/// Types text in the text field. If text already exists in the field, it
+		/// will be cleared.
+		/// </summary>
+		/// <param name="value">
+		/// The text to type in the text field
+		/// </param>
+		public void Type(string value)
+		{
+			_selenium.Type(Locator, value);
+		}
+	}
+}
